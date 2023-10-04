1e12c39369bbd3fdb41da830de4959c0a4b730ef
Jesus7Eduardo <134670273+Jesus7Eduardo@users.noreply.github.com>
2023-10-03 22:02:47 -0600

    Add files via upload

---
+++ Eduardo10AUTT/index.ts
@@ -0,0 +1,11 @@
+const express = require ("express")
+const app = express();
+
+const example = function(){
+    console.log("Jesus Eduardo Esparragoza Rivera 10 A UTTehuacan")
+}
+
+app.listen(
+    9000,
+    example
+)
